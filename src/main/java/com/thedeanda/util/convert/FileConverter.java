31b41fb57d472a5fd7d97840b4d70d043a05f7e4
miguel de anda <miguel@thedeanda.com>
2014-08-04 22:30:01 -0700

    adds in-process version of conversion functions

--- src/main/java/com/thedeanda/util/convert/FileConverter.java
+++ src/main/java/com/thedeanda/util/convert/FileConverter.java
@@ -1,15 +1,18 @@
 package com.thedeanda.util.convert;
 
 import java.io.File;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 
+import com.thedeanda.util.convert.fileinfo.FileInfo;
 import com.thedeanda.util.convert.fileinfo.FileInfoListener;
 import com.thedeanda.util.convert.fileinfo.FileInfoReader;
 import com.thedeanda.util.convert.fileinfo.ImageFileInfo;
 import com.thedeanda.util.convert.image.ImageScaleParams;
 import com.thedeanda.util.convert.image.ImageScaler;
+import com.thedeanda.util.domain.Pointer;
 
 public class FileConverter {
 	private String file = "/usr/bin/file";
@@ -64,8 +67,43 @@ public class FileConverter {
 		executor.execute(new FileInfoReader(this, file, listener));
 	}
 
+	public FileInfo readFileInfoInProcess(File file) {
+		final Pointer<FileInfo> ret = new Pointer<FileInfo>();
+		FileInfoReader fir = new FileInfoReader(this, file,
+				new FileInfoListener() {
+
+					@Override
+					public void fileInfoReady(FileInfo fileInfo) {
+						ret.value = fileInfo;
+					}
+				});
+		fir.run();
+
+		return ret.value;
+	}
+
 	public void convertResize(ImageFileInfo file, ImageScaleParams params,
 			ConversionListener listener) {
 		executor.execute(new ImageScaler(this, file, params, listener));
+	}
+
+	public List<FileInfo> convertResizeInProcess(ImageFileInfo file,
+			ImageScaleParams params) {
+		final Pointer<List<FileInfo>> ret = new Pointer<List<FileInfo>>();
+		ImageScaler is = new ImageScaler(this, file, params,
+				new ConversionListener() {
+
+					@Override
+					public void failed() {
+						ret.value = null;
+					}
+
+					@Override
+					public void complete(List<FileInfo> files) {
+						ret.value = files;
+					}
+				});
+		is.run();
+		return ret.value;
 	}
 }
---
+++ src/main/java/com/thedeanda/util/domain/Pointer.java
@@ -0,0 +1,24 @@
+package com.thedeanda.util.domain;
+
+import java.io.Serializable;
+
+public class Pointer<T> implements Serializable {
+	private static final long serialVersionUID = 1L;
+	public T value;
+
+	public T getValue() {
+		return value;
+	}
+
+	public void setValue(T value) {
+		this.value = value;
+	}
+
+	public Pointer() {
+
+	}
+
+	public Pointer(T value) {
+		this.value = value;
+	}
+}
