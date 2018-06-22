5b74e6fae9556f033f09b1ec996a7b1f25dc4851
miguel de anda <miguel@thedeanda.com>
2018-06-21 23:02:22 -0700

    verifies mp3 and vorbis both work

--- src/main/java/com/thedeanda/util/convert/fileinfo/AudioFileInfo.java
+++ src/main/java/com/thedeanda/util/convert/fileinfo/AudioFileInfo.java
@@ -3,7 +3,6 @@ package com.thedeanda.util.convert.fileinfo;
 public class AudioFileInfo extends FileInfo {
 	private static final long serialVersionUID = 1L;
 	private AudioEncoding encoding;
-	private int lengthInHundredths;
 
 	public AudioEncoding getEncoding() {
 		return encoding;
@@ -11,14 +10,6 @@ public class AudioFileInfo extends FileInfo {
 
 	public void setEncoding(AudioEncoding encoding) {
 		this.encoding = encoding;
-	}
-
-	public int getLengthInHundredths() {
-		return lengthInHundredths;
-	}
-
-	public void setLengthInHundredths(int lengthInHundredths) {
-		this.lengthInHundredths = lengthInHundredths;
 	}
 
 }
--- src/test/java/com/thedeanda/util/convert/TestAudioConversion.java
+++ src/test/java/com/thedeanda/util/convert/TestAudioConversion.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,16 +21,24 @@ import com.thedeanda.util.convert.fileinfo.AudioEncoding;
 import com.thedeanda.util.convert.fileinfo.AudioFileInfo;
 import com.thedeanda.util.convert.fileinfo.FileInfo;
 import com.thedeanda.util.convert.fileinfo.FileInfoListener;
-import com.thedeanda.util.convert.fileinfo.ImageFileInfo;
 
 public class TestAudioConversion {
 
 	private FileConverter convertor;
+	private List<File> files;
 
 	@Before
 	public void init() {
 		convertor = new FileConverter();
 		convertor.setTempDir(new File("tmp"));
+		files = new ArrayList<>();
+	}
+
+	@After
+	public void done() {
+		for (File file : files) {
+			file.delete();
+		}
 	}
 
 	@Test
@@ -57,7 +66,7 @@ public class TestAudioConversion {
 		final CountDownLatch latch2 = new CountDownLatch(1);
 		AudioProperties properties = new AudioProperties();
 		properties.setCodec(AudioCodec.Vorbis);
-		properties.setTargetBaseFilename("foo");
+		properties.setTargetBaseFilename("radiohead");
 		final List<AudioFileInfo> generatedFiles = new ArrayList<>();
 		convertor.convertAudio(fileInfo, properties, new ConversionListener<AudioFileInfo>() {
 			@Override
@@ -67,8 +76,11 @@ public class TestAudioConversion {
 			}
 
 			@Override
-			public void complete(List<AudioFileInfo> files) {
-				generatedFiles.addAll(files);
+			public void complete(List<AudioFileInfo> convertedFiles) {
+				for (AudioFileInfo afi : convertedFiles) {
+					files.add(afi.getFile());
+				}
+				generatedFiles.addAll(convertedFiles);
 				latch2.countDown();
 			}
 		});
@@ -81,6 +93,62 @@ public class TestAudioConversion {
 		AudioFileInfo fi2 = generatedFiles.get(0);
 		assertTrue("file exists", fi2.getFile().exists());
 		assertEquals(AudioEncoding.VORBIS, fi2.getEncoding());
-		//assertEquals(40, fi2.getLengthInHundredths());
+		// assertEquals(40, fi2.getLengthInHundredths());
+	}
+	
+
+	@Test
+	public void testOggToMp3() throws InterruptedException {
+		final CountDownLatch latch = new CountDownLatch(1);
+		final FileInfoHolder holder = new FileInfoHolder();
+
+		File file = new File("src/test/resources/Beck-DeadWildCat.ogg");
+		convertor.readFileInfo(file, new FileInfoListener() {
+			@Override
+			public void fileInfoReady(FileInfo fileInfo) {
+				holder.fileInfo = fileInfo;
+				latch.countDown();
+			}
+		});
+
+		if (!latch.await(10, TimeUnit.SECONDS)) {
+			fail("file info never ready");
+		}
+
+		AudioFileInfo fileInfo = (AudioFileInfo) holder.fileInfo;
+		assertNotNull(fileInfo);
+		assertTrue(fileInfo instanceof AudioFileInfo);
+
+		final CountDownLatch latch2 = new CountDownLatch(1);
+		AudioProperties properties = new AudioProperties();
+		properties.setCodec(AudioCodec.MP3);
+		properties.setTargetBaseFilename("beck");
+		final List<AudioFileInfo> generatedFiles = new ArrayList<>();
+		convertor.convertAudio(fileInfo, properties, new ConversionListener<AudioFileInfo>() {
+			@Override
+			public void failed() {
+				latch2.countDown();
+				fail("failed to convert audio");
+			}
+
+			@Override
+			public void complete(List<AudioFileInfo> convertedFiles) {
+				for (AudioFileInfo afi : convertedFiles) {
+					files.add(afi.getFile());
+				}
+				generatedFiles.addAll(convertedFiles);
+				latch2.countDown();
+			}
+		});
+		if (!latch2.await(30, TimeUnit.SECONDS)) {
+			fail("conversion failed or took too long");
+		}
+
+		assertNotNull(generatedFiles);
+		assertEquals("file count is off", 1, generatedFiles.size());
+		AudioFileInfo fi2 = generatedFiles.get(0);
+		assertTrue("file exists", fi2.getFile().exists());
+		assertEquals(AudioEncoding.MP3, fi2.getEncoding());
+		// assertEquals(40, fi2.getLengthInHundredths());
 	}
 }
